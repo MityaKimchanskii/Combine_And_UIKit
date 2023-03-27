831a2796fbbc2d16afd30738adc272811dce3f98
Dmitrii Kim <mitya_kim@icloud.com>
2023-03-26 17:53:57 -0700

    Combine example 1

--- Combine_And_UIKit/ViewController.swift
+++ Combine_And_UIKit/ViewController.swift
@@ -6,12 +6,43 @@
 //
 
 import UIKit
+import Combine
+
+extension Notification.Name {
+    static let newBlogPost = Notification.Name("newPost")
+}
+
+struct BlogPost {
+    let title: String
+}
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var blogTextField: UITextField!
+    @IBOutlet weak var publishButton: UIButton!
+    @IBOutlet weak var subscribedLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        publishButton.addTarget(self, action: #selector(publishButtonTapped), for: .primaryActionTriggered)
+        
+        // Create a publisher
+        let publisher = NotificationCenter.Publisher(center: .default, name: .newBlogPost, object: nil)
+            .map { (notification) -> String? in // Combine with an operator
+            return (notification.object as? BlogPost)?.title ?? ""
+        }
+        
+        // Create a subscriber
+        let subscriber = Subscribers.Assign(object: subscribedLabel, keyPath: \.text)
+        publisher.subscribe(subscriber)
+    }
+    
+    @objc func publishButtonTapped(_ sender: UIButton) {
+        // Post the notification
+        let title = blogTextField.text ?? "Coming soon"
+        let blogPost = BlogPost(title: title)
+        NotificationCenter.default.post(name: .newBlogPost, object: blogPost)
     }
 
 
